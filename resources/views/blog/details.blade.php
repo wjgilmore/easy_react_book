3566fa19a7ee2247346304fd9ad21ff188132992
Jason Gilmore <wj@wjgilmore.com>
2015-06-15 15:45:14 -0400

    Added link to TOC in blog details header

--- resources/views/blog/details.blade.php
+++ resources/views/blog/details.blade.php
@@ -10,7 +10,7 @@
             <div class="col-sm-10 col-sm-offset-1 details">
 
                 <p>
-                <em>The following blog post is an adapted excerpt from the forthcoming book, "Easy React: Build Powerful Web Apps Using Modern JavaScript Technologies", by W. Jason Gilmore. </em>
+                <em>The following blog post is an adapted excerpt from the forthcoming book, "Easy React: Build Powerful Web Apps Using Modern JavaScript Technologies", by W. Jason Gilmore. See the <a href="/toc">table of contents</a> to read the introduction and chapter 1 online for free!</em>
                 </p>
 
                 {!! $article['content'] !!}
