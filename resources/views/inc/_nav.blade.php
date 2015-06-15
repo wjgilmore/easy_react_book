9f59d3ee602ca5055506cc4d982eba684fd3c945
Jason Gilmore <wj@wjgilmore.com>
2015-06-15 15:41:19 -0400

    Layout nav fix

--- resources/views/inc/_nav.blade.php
+++ resources/views/inc/_nav.blade.php
@@ -2,6 +2,6 @@
   <li><strong><a href="/">Easy React: Build Powerful Web Apps Using Modern JavaScript Technologies</a></strong> / </li>
   <li><a href="/toc">Table of Contents</a> / </li>
   <li><a href="/project">The Companion Project</a> / </li>
-  <li><a href="/author">About the Author</a></li>
+  <li><a href="/author">About the Author</a> / </li>
   <li><a href="/blog">Blog</a></li>
 </ul>
